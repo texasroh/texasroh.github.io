202bc0714adefe1f2d0a6e2243cb8de48ebf7a0c
June Roh <texasroh@gmail.com>
2022-01-30 03:57:05 -0500

    init

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,11 @@
-function startUp() {
-	document.querySelector('.header').innerHTML = "Hello World";
-}
-
-startUp();
+document.querySelectorAll("*[data-include-path]").forEach((element) => {
+	var includePath = element.dataset.includePath;
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200){
+			element.outerHTML = this.responseText;
+		}
+	};
+	xhttp.open('GET', includePath, true);
+	xhttp.send();
+});
