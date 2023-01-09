a27b718d6e6b6428cac433c8d51d0b0479d2dce8
texasroh <texasroh@gmail.com>
2023-01-09 00:27:35 -0500

    fill education section

--- src/sections/home/Educations.tsx
+++ src/sections/home/Educations.tsx
@@ -3,8 +3,27 @@ import HomeSection from "../../components/HomeSection";
 const Educations = () => {
   return (
     <HomeSection title="Educations">
-      <div>University of Texas</div>
-      <div>경북대학교</div>
+      <div className="space-y-4">
+        <div>
+          <div className="flex justify-between font-medium">
+            <div>University of Texas</div>
+            <div>2016.01 ~ 2020.05</div>
+          </div>
+          <ul className="ml-6">
+            <li>Business Analytics</li>
+            <li>Master of Business Administration</li>
+          </ul>
+        </div>
+        <div>
+          <div className="flex justify-between font-medium">
+            <div>경북대학교</div>
+            <div>2007.03 ~ 2014.02</div>
+          </div>
+          <ul className="ml-6">
+            <li>전자공학부</li>
+          </ul>
+        </div>
+      </div>
     </HomeSection>
   );
 };
