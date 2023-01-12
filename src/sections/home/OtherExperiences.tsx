c81c50ea881e6549840fde19237b61d10c9dbf98
texasroh <texasroh@gmail.com>
2023-01-12 01:15:15 -0500

    finalize toy project section

--- src/sections/home/OtherExperiences.tsx
+++ src/sections/home/OtherExperiences.tsx
@@ -1,13 +1,17 @@
 import HomeSection from "../../components/HomeSection";
+import { FaGithub } from "react-icons/fa";
 
 const OtherExperiences = () => {
   return (
     <HomeSection title="Other Experience">
       <ul className="ml-4 list-disc space-y-4 md:ml-0">
         <li>
-          <div>
+          <div className="flex items-center space-x-2">
             <a href="https://texasroh.github.io/muraVPN" target="_blank">
               VPN Service
+            </a>
+            <a href="https://github.com/texasroh/muraVPN" target="_blank">
+              <FaGithub />
             </a>
           </div>
           <p>해외에서 한국의 서비스를 이용하기 위한 VPN 서비스</p>
--- src/sections/home/ToyProjects.tsx
+++ src/sections/home/ToyProjects.tsx
@@ -1,15 +1,52 @@
 import HomeSection from "../../components/HomeSection";
+import { FaGithub } from "react-icons/fa";
 
 const ToyProjects = () => {
   return (
     <HomeSection title="Toy Projects">
-      <div>hello</div>
-      <div>hello</div>
-      <div>hello</div>
-      <div>hello</div>
-      <div>hello</div>
-      <div>hello</div>
-      <div>hello</div>
+      <ul className="ml-4 list-disc space-y-4 md:ml-0">
+        <li>
+          <div className="flex items-center space-x-2">
+            <h4 className="text-lg font-medium">
+              <a href="https://dmvhanin.com" target="_blank" className="inline">
+                dmvhanin.com
+              </a>
+            </h4>
+            <FaGithub />
+            <span>
+              <a href="https://github.com/texasroh/dmvhanin" target="_blank">
+                V1
+              </a>
+            </span>
+            ,
+            <span>
+              <a href="https://github.com/texasroh/dmvhanin-v2" target="_blank">
+                V2
+              </a>
+            </span>
+          </div>
+          <p>버지니아, 디씨, 메릴랜드 한인 커뮤니티 사이트</p>
+          <p>Flask(V1), Django(V2), EC2, RDS, Nginx, Gunicorn, PostgreSQL</p>
+        </li>
+        <li>
+          <div className="flex items-center space-x-2">
+            <h4 className="text-lg font-medium">
+              <a
+                href="https://play.google.com/store/apps/details?id=com.pricescanner"
+                target="_blank"
+                className="inline"
+              >
+                Price Scanner
+              </a>
+            </h4>
+            <a href="https://github.com/texasroh/price-scanner" target="_blank">
+              <FaGithub />
+            </a>
+          </div>
+          <p>오프라인에서 쇼핑할때 바코드 스캔으로 온라인 쇼핑몰 가격 비교</p>
+          <p>React Native, cheerio</p>
+        </li>
+      </ul>
     </HomeSection>
   );
 };
