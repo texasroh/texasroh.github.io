0a3d8512cc0e46f3d560640f741c70cc3b6e0759
texasroh <texasroh@gmail.com>
2023-01-13 01:37:06 -0500

    add experiences and intro

--- src/sections/home/Experiences.tsx
+++ src/sections/home/Experiences.tsx
@@ -19,13 +19,24 @@ const Experiences = () => {
             <li className="flex flex-col md:flex-row">
               <div className="basis-1/4 font-bold">Tech Lead</div>
               <ul className="ml-4 list-disc">
-                <li>온라인 </li>
+                <li>
+                  온라인 밀키트 사이트 및 백오피스 개발. 새로운 비지니스 창출
+                  (shopify, OAuth, Flask)
+                </li>
+                <li>
+                  운영팀, 회계팀을 위한 ERP 시스템 개발 (Django, React, React
+                  Native)
+                </li>
+                <li>
+                  모바일로 실시간 레포트를 보여줄 수 있는 대시보드 개발 (Flask)
+                </li>
               </ul>
             </li>
             <li className="flex flex-col md:flex-row">
               <div className="basis-1/4 font-bold">Data Lead</div>
               <ul className="ml-4 list-disc">
-                <li></li>
+                <li>ETL 자동화 시스템</li>
+                <li>수요 예측과 새로운 상품도입에 관한 분석</li>
               </ul>
             </li>
           </ul>
